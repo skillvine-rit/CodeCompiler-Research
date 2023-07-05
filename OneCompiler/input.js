dd9afd638ff748189451dca240fbb8c2a740619f
alvin <alvin19official@gmail.com>
2023-07-05 21:00:11 +0530

    first commit

---
+++ OneCompiler/input.js
@@ -0,0 +1,32 @@
+let input = {
+  _id: "3zdmtthtf",
+  type: "code",
+  created: "2023-07-05T15:12:37.462Z",
+  updated: "2023-07-05T15:13:06.953Z",
+  title: "3zdmtthtf",
+  description: null,
+  tags: [],
+  visibility: "public",
+  properties: {
+    language: "python",
+    files: [
+      {
+        name: "main.py",
+        content: 'x = int(input("oom: "))\nprint(x)',
+      },
+    ],
+    stdin: "19",
+    hash: "c3c05c55a2cee0c12ea0828a4493b009ae592039",
+    result: {
+      stdout: "HI da : ok\n7\n",
+      stderr: null,
+      exception: null,
+      executionTime: 36,
+      success: true,
+      output: "HI da : ok\n7\n",
+    },
+  },
+  user: null,
+  idToken:
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjN6ZG10dGh0ZiIsImlhdCI6MTY4ODU3MDIyMH0.FyUR5WaRdi_CaVZiTLFj2DnxqM5taVwgQ1ED1pIZ1xc",
+};
